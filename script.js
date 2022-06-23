cd9bb81eca6d84b58e09f3056e003ebb95796990
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-06-23 14:27:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = "Asuquo";
+let myHeight = "5\'11";
+let country = "Nigeria"
+console.log("My name is " + myName);
+console.log("My height is " + myHeight);
+console.log("My Country is " + country);
+
