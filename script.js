1c132f62c3f4947c769f2803b360c25f64ee3b76
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-06-27 12:02:30 +0100

    changed console.log to alert

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myName = "Asuquo";
 let myHeight = "5\'11";
 let country = "Nigeria";
-
+alert("My name is " + myName + "." + "\nI am " + myHeight + " feet tall." + "\nMy Country is " + country + "." );
 
 
